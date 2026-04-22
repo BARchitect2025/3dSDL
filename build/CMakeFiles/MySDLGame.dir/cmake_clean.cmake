622fc402f5e627cc623a7bb0fe86673a7305d7f5
BARchitect <barchitect2025@gmail.com>
2026-04-22 09:51:34 -0400

    Got CMake working

---
+++ build/CMakeFiles/MySDLGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySDLGame.dir/main.cpp.o"
+  "CMakeFiles/MySDLGame.dir/main.cpp.o.d"
+  "MySDLGame"
+  "MySDLGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySDLGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
